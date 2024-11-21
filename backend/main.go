d9f799390f735412d8ee9b1f217ce396a7eebbb6
suresh-02 <97348927+suresh-02@users.noreply.github.com>
2024-11-21 12:32:24 +0530

    Initilazied env files and setted up a simple route

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/suresh-02/Iragu-booking/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"age":     "200",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
 }
-	
